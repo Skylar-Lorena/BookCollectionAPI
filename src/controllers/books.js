1d9d0195e373c0db36d7a51a3f0f91a450cb8b5d
Skylar-Lorena <lorayne911@gmail.com>
2024-08-19 13:50:55 +0300

    implement the remaining controller functions for creating, retrieving, updating, and deleting books

---
+++ src/controllers/books.js
@@ -0,0 +1,63 @@
+const Book = require('../models/Book');
+
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createBook = async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    isbn: req.body.isbn,
+    publishedYear: req.body.publishedYear,
+    description: req.body.description,
+  });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateBook = async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    if (!updatedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.deleteBook = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
